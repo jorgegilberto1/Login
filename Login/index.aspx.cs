ae3235dd6ec7b16c915ddef1c008bac734051806
jorgegilberto1 <jrgbp@hotmail.com>
2022-05-25 09:32:23 -0500

    Add project files.

---
+++ Login/index.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Login
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["usuariologueado"] != null)
+            {
+                string usuariologueado = Session["usuariologueado"].ToString();
+                lblBienvenida.Text = "Bienvenido/a " + usuariologueado;
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+        protected void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Session.Remove("usuariologueado");
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
